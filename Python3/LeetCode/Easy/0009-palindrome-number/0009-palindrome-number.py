0b07ff8f6513abc430344a576de59dae20e849dc
Donghyun Nam <85597534+namudongs@users.noreply.github.com>
2025-01-20 19:01:15 +0900

    [Easy] Time: 4 ms (80.61%), Space: 17.7 MB (36.85%) -LeetHub

---
+++ Python3/LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        
+        reversedHalf = 0
+        original = x 
+        
+        while x > reversedHalf:
+            reversedHalf = reversedHalf * 10 + (x % 10)
+            x //= 10
+        
+        return x == reversedHalf or x == reversedHalf // 10
