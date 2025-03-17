8be6ad89854e3e75ecffa2f9d570cfe7e8b96bf7
Donghyun Nam <85597534+namudongs@users.noreply.github.com>
2025-03-17 13:27:25 +0900

    [level 1] Title: 덧칠하기, Time: 1.74 ms, Memory: 17.9 MB -BaekjoonHub

--- Swift/프로그래머스/1/161989. 덧칠하기/덧칠하기.swift
+++ Swift/프로그래머스/1/161989. 덧칠하기/덧칠하기.swift
@@ -1,23 +1,14 @@
-import Foundation
-
-func solution(_ n:Int, _ m:Int, _ section:[Int]) -> Int {
-    var allSection = Array(repeating: true, count: n+1)
-    for value in section {
-        allSection[value] = false
-    }
-    
-    var i = 1
+func solution(_ n: Int, _ m: Int, _ section: [Int]) -> Int {
     var result = 0
+    var i = 0
     
-    while i < allSection.count {
-        if allSection[i] == false {
-            for j in i..<min(i+m, allSection.count) {
-                allSection[j] = true
-            }
-            result += 1
-        }
+    while i < section.count {
+        result += 1
+        let start = section[i]
         
-        i += 1
+        while i < section.count && section[i] < start + m {
+            i += 1
+        }
     }
     
     return result
