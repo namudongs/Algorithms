90a37e81c6388bd075725a9e574c124b64c2bdd7
Donghyun Nam <85597534+namudongs@users.noreply.github.com>
2025-04-05 17:02:14 +0900

    [Bronze III] Title: 이진수, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- Swift/백준/Bronze/3460. 이진수/이진수.swift
+++ Swift/백준/Bronze/3460. 이진수/이진수.swift
@@ -1,22 +1,12 @@
-import Foundation
-
-func solution() {
-    let t = Int(readLine()!)!
-    let arr = (0..<t).map { _ in Int(readLine()!)! }
-    
-    let radixArr = (0..<arr.count).map { i in
-        String(arr[i], radix: 2).reversed().map { String($0) }
-    }
-    
-    for binary in radixArr {
-        let result = binary.enumerated().filter {
-            $0.element == "1"
-        }.map {
-            "\($0.offset)"
+if let t = Int(readLine()!) {
+    for _ in 0..<t {
+        if let n = Int(readLine()!) {
+            let binary = String(n, radix: 2)
+            let positions = binary.reversed().enumerated()
+                .filter { $0.element == "1" }
+                .map { String($0.offset) }
+            
+            print(positions.joined(separator: " "))
         }
-        
-        print(result.joined(separator: " "))
     }
 }
-
-solution()
