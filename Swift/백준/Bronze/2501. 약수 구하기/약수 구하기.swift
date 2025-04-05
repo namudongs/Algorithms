c88d446ef1af9145c06fdd3553522853454af128
Donghyun Nam <85597534+namudongs@users.noreply.github.com>
2025-04-05 16:47:56 +0900

    [Bronze III] Title: 약수 구하기, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

--- Swift/백준/Bronze/2501. 약수 구하기/약수 구하기.swift
+++ Swift/백준/Bronze/2501. 약수 구하기/약수 구하기.swift
@@ -1,19 +1,17 @@
 import Foundation
 
-func solution() {
-    let input = readLine()!.split(separator: " ").compactMap { Int(String($0)) }
-    let n = input[0], k = input[1] - 1
-    
-    var result = [Int]()
-    
-    for i in 1...n {
+func solution() -> Int {
+    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let n = input[0], k = input[1] // n의 약수 중 k번째로 작은 수 출력
+    var result = [Int](repeating: 1, count: 1)
+
+    for i in 2...n {
         if n % i == 0 {
             result.append(i)
         }
     }
-    
-    guard result.count > k else { print(0); return }
-    print(result.sorted(by: <)[k])
+
+    return result.count < k ? 0 : result[k-1]
 }
 
-solution()
+print(solution())
